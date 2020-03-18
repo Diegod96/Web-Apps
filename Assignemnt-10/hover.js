17f878951c61057f69fb0720c34112866a0208dd
Diegod96 <ddphillyfan@gmail.com>
2020-03-17 22:25:28 -0400

    #1 done

---
+++ Assignemnt-10/hover.js
@@ -0,0 +1,11 @@
+
+
+
+function setNewImage(){
+    
+    document.getElementById("img1").src="images/07car1.jpg";
+}
+
+function setOldImage(){
+    document.getElementById("img1").src="images/2019_genesis_g80_angularfront.jpg"
+}
